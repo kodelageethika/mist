847e834ea279abe0080dbace76cb8eb7e6225e0a
sahasra1209 <sahasrakethineedi@gmail.com>
2026-01-09 19:15:22 +0530

    Remove Firebase API key from source code

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,16 @@ import { getFirestore } from "firebase/firestore";
 
 
  const firebaseConfig = {
-    apiKey: "AIzaSyAGPLOVwUhov3_8F7Iv5TdzAov_nSwkK0A",
-    authDomain: "chat-7077a.firebaseapp.com",
-    databaseURL: "https://chat-7077a-default-rtdb.firebaseio.com",
-    projectId: "chat-7077a",
-    storageBucket: "chat-7077a.firebasestorage.app",
-    messagingSenderId: "102903516343",
-    appId: "1:102903516343:web:0f4ebbca61785460711132",
-    measurementId: "G-6ERD548TXY" 
-    };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
 
 const app = initializeApp(firebaseConfig);
 const googleProvider = new GoogleAuthProvider();
